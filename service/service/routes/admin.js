c23f3c9ed2b5336690b12750536514fc07d2b212
pathum-di <pathum9868@gmail.com>
2018-11-18 20:19:46 +0530

    admin sample view

--- service/service/routes/admin.js
+++ service/service/routes/admin.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var expressValidator = require('express-validator');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  database: 'servicepoladb',
+  user: 'root',
+  password: '',
+});
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.render('admin/admin_home',{ layout: 'admin_layout' });
+  connection.query('SELECT * FROM service_provider', function (err, allusers) {
+    // console.log(allusers);
+    res.render('admin/admin_home', { allusers: allusers, layout: 'admin_layout' });
   })
-  
-  module.exports = router;
+});
+
+router.get('/all_user/more/:id', function (req, res, next) {
+  var id=req.params.id;
+  res.send('id is '+ id);
+});
+
+module.exports = router;
